1b68f6dd0f263e94c8780809a62d0b871761c069
Matthew Pagan <mastapegs01@gmail.com>
2021-12-17 22:11:40 -0500

    test adding width and height

--- src/webgl/webglApp.ts
+++ src/webgl/webglApp.ts
@@ -15,9 +15,6 @@ export function startApp(canvas: HTMLCanvasElement) {
   const gl = canvas.getContext("webgl")!;
   const rectangleProgram = createProgram(gl, vertexSource, fragmentSource);
 
-  resizeCanvasToDisplaySize(canvas);
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
   // Lookup attribute and uniform locations
   const positionAttributeLocation = gl.getAttribLocation(
     rectangleProgram,
