3be842db6b05fb9f9c70d8e1fc22297de17f4f9f
Matthew Pagan <mastapegs01@gmail.com>
2021-12-19 15:08:41 -0500

    canvas ready

--- src/components/webgl-app.ts
+++ src/components/webgl-app.ts
@@ -1,12 +1,24 @@
-import { html, css, LitElement } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { html, LitElement } from "lit";
+import { customElement, query } from "lit/decorators.js";
 
 @customElement("webgl-app")
 export class WebGLApp extends LitElement {
+  @query("canvas") canvas!: HTMLCanvasElement;
+
+  initializeWebGL() {
+    console.log(this.canvas);
+  }
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    setTimeout(() => this.initializeWebGL());
+  }
+
   render() {
     return html`
       <canvas></canvas>
       <p>UI Here</p>
+      ${console.log("render() called")}
     `;
   }
 }
