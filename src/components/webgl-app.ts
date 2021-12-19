f8465387fb52d5b10f11ea93b759675cc5e86eef
Matthew Pagan <mastapegs01@gmail.com>
2021-12-19 17:32:13 -0500

    simplify shader

--- src/components/webgl-app.ts
+++ src/components/webgl-app.ts
@@ -61,7 +61,8 @@ export class WebGLApp extends LitElement {
 
       void main() {
         // convert from clipspace -1,1 -> colorspace 0,1
-        vec2 color_space = (vec2(10, 10) * v_position + vec2(1, 1)) / vec2(2, 2);
+        vec2 shrink_color = vec2(10, 10);
+        vec2 color_space = (shrink_color * v_position + vec2(1, 1)) / vec2(2, 2);
 
         gl_FragColor = vec4(color_space, 0.7, 1);
       }
