d436d0f39a7e21058ec6e7dadf13e975050eb70c
Matthew Pagan <mastapegs01@gmail.com>
2021-12-20 13:38:05 -0500

    translation all set

--- src/components/webgl-app/fragment.glsl
+++ src/components/webgl-app/fragment.glsl
@@ -2,8 +2,10 @@ precision mediump float;
 varying vec2 v_position;
 
 void main() {
+  float color_scale = 0.01;
+  vec2 shrink_color = vec2(color_scale, color_scale);
+
   // convert from clipspace -1,1 -> colorspace 0,1
-  vec2 shrink_color = vec2(10, 10);
   vec2 color_space = (shrink_color * v_position + vec2(1, 1)) / vec2(2, 2);
 
   gl_FragColor = vec4(color_space, 0.7, 1);
