d14857ba33a061355ced0736bc44a0cb62f766c9
Matthew Pagan <mastapegs01@gmail.com>
2021-12-21 22:29:04 -0500

    tweak z factor

--- src/components/webgl-3d/vertex.glsl
+++ src/components/webgl-3d/vertex.glsl
@@ -8,7 +8,7 @@ varying vec4 v_color;
 void main() {
   vec4 position = u_matrix * vec4(a_position, 1);
 
-  float fudgeFactor = 1.5;
+  float fudgeFactor = 1.2;
   float zToDivideBy = 1.0 + position.z * fudgeFactor;
   
   gl_Position = vec4(position.xy * zToDivideBy, position.zw);
